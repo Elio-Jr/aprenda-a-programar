b4ab736d181e678f221c0cbf6a84e9dcf071410e
Elio Jr <elio.junior.net@gmail.com>
2022-08-25 06:18:08 -0300

    Agência de turismo - 2a versão...

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,10 +1,11 @@
 let nome, email, profissao, idade, lugar;
 
-nome  = 'Elio Jr.';
-email = 'elio.junior.net@gmail.com';
+nome      = 'Elio Jr.';
+email     = 'elio.junior.net@gmail.com';
 profissao = 'Dev.';
-idade = 45;
-lugar = 'E.U.A.';
+idade     = 45;
+lugar     = 'E.U.A.';
 
-console.log('Nome:', nome, '|eMail:', email, '|Profissão:', profissao, '|Idade:', idade, '|Lugar:', lugar);
+console.log('\n*** Lista de variáveis: ***\nNome:', nome, '\neMail:', email, '\nProfissão:', profissao, '\nIdade:', idade, '\nLugar:', lugar, '\n');
 
+console.log("Olá,", nome, "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para", lugar, ". Confirma pra gente alguns dados? A sua idade é", idade, ", você é", profissao, "e o seu email é", email, "? Ficamos no aguardo. Muito obrigado e boa viagem! :D");
