26a132c0204a29837325689bdf8870f2e1a3f987
Elio Jr <elio.junior.net@gmail.com>
2022-08-25 05:20:19 -0300

    Agência de turismo - 1a versão...

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,2 +1,10 @@
 let nome, email, profissao, idade, lugar;
 
+nome  = 'Elio Jr.';
+email = 'elio.junior.net@gmail.com';
+profissao = 'Dev.';
+idade = 45;
+lugar = 'E.U.A.';
+
+console.log('Nome:', nome, '|eMail:', email, '|Profissão:', profissao, '|Idade:', idade, '|Lugar:', lugar);
+
