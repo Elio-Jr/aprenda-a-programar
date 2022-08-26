8e680dd11eeb14ebf6dd43dad4990b996b866551
Elio Jr <elio.junior.net@gmail.com>
2022-08-26 17:07:06 -0300

    Loops -1a versão...

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,6 @@
+let cont = 1, n = 10;
+
+while (cont <= n)  {
+    console.log(cont);
+    cont ++;
+}
---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+let cont = 1, tabuada = 9;
+
+console.log(`
+Tabuada do ${tabuada}
+===================`);
+
+while (cont <= 10) {
+    console.log(`${tabuada} x ${cont} = ${tabuada * cont}`);
+    cont ++;
+}
